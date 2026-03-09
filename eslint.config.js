597592d36f72569de513ae7c5b944dd1a66ea216
Tim Wickstrom <tim@wickstrom.me>
2026-03-09 00:27:50 -0500

    chore: migrate to eslint flat config

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        setTimeout: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
+  {
+    ignores: ["node_modules/", "dist/"],
+  },
+];
--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   semi: true,
   singleQuote: false,
   tabWidth: 2,
